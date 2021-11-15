9fbebac50a778303418fe5b1c25cf0877896a89d
kosmodev <laurellicosmo@gmail.com>
2021-11-15 16:03:51 +0100

    adds errorBoundry components and apply on cardList

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 const SearchBox = ({ searchChange }) => {
   return (
-    <div className='pa2' >
+    <div className="pa2">
       <input
-        className='pa3 ba b--green bg-lightest-blue '
+        className="pa3 ba b--green bg-light-green"
         type="search"
-        placeholder='search robots'
+        placeholder="search robots"
         onChange={searchChange}
       />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBox
+export default SearchBox;
